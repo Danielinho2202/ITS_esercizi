89a06e81e78fb66ec13c06dfd07906cd0d0c9676
Danielinho2202 <daniele.gallo2202@gmail.com>
2025-02-17 19:31:30 +0100

    first-commit

---
+++ lezione_01/es_lezione2.py
@@ -0,0 +1,13 @@
+#es.2-3
+(name) = str(input("inserisci il tuo nome"))
+print (f"Hey {name} , where are you from?")
+#2-4
+print(name.lower())  
+print(name.upper()) 
+print(name.title()) 
+#2-5
+quote:str = ("i like computers!")
+famous_person = ("Daniele")
+print(f"{famous_person} disse: {quote}" )
+
+

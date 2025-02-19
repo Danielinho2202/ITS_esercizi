13b2d69386f6e14b671c8b32a72414102b9f50f2
Danielinho2202 <daniele.gallo2202@gmail.com>
2025-02-19 19:24:41 +0100

    esercizi in classe 4

---
+++ lezione2/es_lezione2_3-4-5.py
@@ -0,0 +1,6 @@
+#3.4
+name:list= ["daniele", "pietro", "giorgia"]
+print (f"Hey {name[0]}, vuoi venire alla cena?" )
+print (f"Hey {name[1]}, vuoi venire alla cena?" )
+print (f"Hey {name[2]}, vuoi venire alla cena?" )
+#3.5
